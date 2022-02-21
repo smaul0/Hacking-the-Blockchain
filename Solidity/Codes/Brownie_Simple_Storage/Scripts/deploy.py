f22df3ac2c1858d28b2e90a5d5be1e8d5769beef
smaul0 <80347095+smaul0@users.noreply.github.com>
2022-02-21 21:28:46 +0530

    Add files via upload

--- Solidity/Codes/Brownie_Simple_Storage/Scripts/deploy.py
+++ Solidity/Codes/Brownie_Simple_Storage/Scripts/deploy.py
@@ -1,1 +1,22 @@
+from brownie import accounts, config, network, SimpleStorage
 
+
+def deploy_simple_storage():
+    account = get_account()
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    update_store_value = simple_storage.store(15, {"from": account})
+    update_store_value.wait(1)
+    print(f"updated stored value {simple_storage.retrieve()}")
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["form_key"])
+
+
+def main():
+    deploy_simple_storage()
